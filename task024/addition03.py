29377fff2cb80586139ddeb8bb2d8970aa1ad4e7
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-09 16:47:16 +1000

    changed addition03.py

--- task024/addition03.py
+++ task024/addition03.py
@@ -1,41 +1,22 @@
-# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
-# Пример:
-# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+# Напишите программу, которая
+# 1. Принимает на ввод последовательность чисел. 
+# 2. Выведет список неповторяющихся элементов исходной последовательности.
 
-from random import randint
+print('Введите числа через пробел')
+numbers = [int(x) for x in input().split()]
 
-k = int(input('Введите натуральную степень k: '))
-while k < 1:
-    print('Введеные неверные данные')
-    k = int(input('Введите натуральную степень k: '))
+numbers_counter = dict()
+for i in numbers:
+    numbers_counter[i] = 0
+for i in numbers:
+    numbers_counter[i] +=1
+# print(numbers_counter)
 
-degrees = []
-for i in range(1, k+1):
-    degrees.append(i)
+unique_numbers = list()
 
-coeffs = []
-for i in range(k):
-    coeffs.append(randint(0, 10))
+for i in numbers_counter:
+    if numbers_counter[i] == 1:
+        unique_numbers.append(i)
 
-# expression = 0
-expression_string = ''
-c = randint(0, 10)
-
-for i in range(k):
-    if coeffs[i] > 0:
-        expression_string += f'{coeffs[i]}*x^{degrees[i]} + '
-    # expression += coeffs[i]*x**degrees[i]
-
-if expression_string != '':
-    if c != 0:
-        expression_string += f'{c}'
-    else:
-        expression_string = expression_string[:-3]
-    expression_string += ' = 0'
-    print(expression_string)
-else:
-    print('Все коэффиценты многочлена равны 0')
-# expression += c
-
-with open('file.txt', 'w') as data:
-     data.write(expression_string)
+print('Неповторяющиеся числа')
+print(*unique_numbers)
