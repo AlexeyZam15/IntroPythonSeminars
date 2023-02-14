b3f797c2383df342568e456897b5da75fea05910
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-14 17:39:50 +1000

    changed task026

--- task026/main.py
+++ task026/main.py
@@ -2,18 +2,16 @@
 # два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 
-a = int(input('Введите число: '))
-b = int(input('Введите степень: '))
-
-
 def degree_f(number, degree):
-
     if degree > 0:
         number *= number
-        degree_f(number, degree-1)
+        degree_f(number, degree - 1)
     if degree == 0:
         return 1
     return number
 
+
+a = int(input('Введите число: '))
+b = int(input('Введите степень: '))
 
 print(f'{a}^{b} =', degree_f(a, b))
