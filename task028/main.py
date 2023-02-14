c65eb09e57220032c8b8e0e80c8e6b84502a9278
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-14 17:39:57 +1000

    changed task028

--- task028/main.py
+++ task028/main.py
@@ -3,14 +3,13 @@
 # всех арифметических операций допускаются только +1 и -1.
 # Также нельзя использовать циклы.
 
+def sum_numbers(max1, min1):
+    if min1 > 0:
+        max1 = sum_numbers(max1 + 1, min1 - 1)
+    return max1
+
+
 a1 = int(input('Введите первое число: '))
 b1 = int(input('Введите второе число: '))
-
-
-def sum_numbers(max, min):
-    if min > 0:
-        max = sum_numbers(max+1, min-1)
-    return max
-
 
 print('Сумма чисел =', sum_numbers(max(a1, b1), min(a1, b1)))
