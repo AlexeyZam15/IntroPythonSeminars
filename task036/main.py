e9a0b9a595e35406abc91f22039a90c36150735a
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-18 04:35:39 +1000

    changed task036

--- task036/main.py
+++ task036/main.py
@@ -8,23 +8,34 @@
 # Ввод:
 # print_operation_table(lambda x, y: x * y)
 # Вывод:
-#  1 2 3 4 5 6
-#  2 4 6 8 10 12
-#  3 6 9 12 15 18
-#  4 8 12 16 20 24
-#  5 10 15 20 25 30
-#  6 12 18 24 30 36
+#  1   2   3   4   5   6
+#  2   4   6   8  10  12
+#  3   6   9  12  15  18
+#  4   8  12  16  20  24
+#  5  10  15  20  25  30
+#  6  12  18  24  30  36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    # text = ''
+    # table = ''
     # for i in range(1, num_rows + 1):
     #     for j in range(1, num_columns + 1):
-    #         text += f'{j * i:4}'
-    #     text += '\n'
-    # #     text += ''.join([f'{j * i:4}' for j in range(1, num_columns + 1)]) + '\n'
-    mult_table = '\n'.join(
-        list(map(lambda x: ''.join([f'{j * x:4}' for j in range(1, num_columns + 1)]), range(1, num_rows + 1))))
-    print(mult_table)
+    #         if j == 1:
+    #             table += f'{i:<5}'
+    #         elif i == 1:
+    #             table += f'{j:<5}'
+    #         else:
+    #             table += f'{abs(operation(i, j)):<5}'
+    #     table += '\n'
+    #     table += ''.join([f'{abs(operation(i, j)):<5}'
+    #                       if j != 1 and i != 1 else f'{i:<5}'
+    #                       if j == 1 else f'{j:<5}'
+    #                       for j in range(1, num_columns + 1)]) + '\n'
+    table = '\n'.join(
+        list(map(lambda i: ''.join([f'{abs(operation(i, j)):<5}'
+                                    if j != 1 and i != 1 else f'{i:<5}'
+                                    if j == 1 else f'{j:<5}'
+                                    for j in range(1, num_columns + 1)]), range(1, num_rows + 1))))
+    print(table)
 
 
 rows, columns = [int(x) for x in input('Введите кол-во колонок и столбцов через пробел: ').split()[:2]]
