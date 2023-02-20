ef5ca4058bfd9d54884aa0c6d5c3250b53d403fa
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-21 03:39:18 +1000

    changed task036

--- task036/main.py
+++ task036/main.py
@@ -15,6 +15,9 @@
 #  5  10  15  20  25  30
 #  6  12  18  24  30  36
 
+import time
+
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
     # table = ''
     # for i in range(1, num_rows + 1):
@@ -26,16 +29,41 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
     #         else:
     #             table += f'{abs(operation(i, j)):<5}'
     #     table += '\n'
-    #     table += ''.join([f'{abs(operation(i, j)):<5}'
-    #                       if j != 1 and i != 1 else f'{i:<5}'
-    #                       if j == 1 else f'{j:<5}'
-    #                       for j in range(1, num_columns + 1)]) + '\n'
+    # #     table += ''.join([f'{abs(operation(i, j)):<5}'
+    # #                       if j != 1 and i != 1 else f'{i:<5}'
+    # #                       if j == 1 else f'{j:<5}'
+    # #                       for j in range(1, num_columns + 1)]) + '\n'
+    #
+    # start = time.time()
+
     table = '\n'.join(
-        map(lambda i: ''.join([f'{abs(operation(i, j)):<5}'
-                               if j != 1 and i != 1 else f'{i:<5}'
-                               if j == 1 else f'{j:<5}'
+        map(lambda i: ''.join([f'{operation(i, j):5}'
+                               if j != 1 and i != 1 else f'{i:5}'
+        if j == 1 else f'{j:5}'
                                for j in range(1, num_columns + 1)]), range(1, num_rows + 1)))
     print(table)
+
+    # time1 = time.time() - start
+    #
+    # start = time.time()
+    #
+    # table = list(
+    #     map(lambda i: [operation(i, j)
+    #                    if j != 1 and i != 1 else i
+    #                    if j == 1 else j
+    #                    for j in range(1, num_columns + 1)], range(1, num_rows + 1)))
+    # # for i in table:
+    # #     for j in i:
+    # #         print(f'{j:<5}', end='')
+    # #     print()
+    # list(map(lambda i: [print(f'{j:5}', end='') for j in i] + [print()], table))
+    #
+    # time2 = time.time() - start
+    #
+    # timers = [time1, time2]
+    #
+    # print(*timers)
+    # print(f'№ {timers.index(min(timers)) + 1} быстрее')
 
 
 rows, columns = [int(x) for x in input('Введите кол-во колонок и столбцов через пробел: ').split()[:2]]
