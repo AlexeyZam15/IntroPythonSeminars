bc86edce4e6a2f1e14a01107e22e667e07fe11e1
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-21 16:40:04 +1000

    changed task036

--- task036/main.py
+++ task036/main.py
@@ -39,7 +39,7 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
     table = '\n'.join(
         map(lambda i: ''.join([f'{operation(i, j):5}'
                                if j != 1 and i != 1 else f'{i:5}'
-        if j == 1 else f'{j:5}'
+                               if j == 1 else f'{j:5}'
                                for j in range(1, num_columns + 1)]), range(1, num_rows + 1)))
     print(table)
 
