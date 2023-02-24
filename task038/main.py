4e8cadaf3b20ed9d46173accc6d09b28c70eb51f
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-02-24 19:59:26 +1000

    changed task038

--- task038/main.py
+++ task038/main.py
@@ -6,7 +6,7 @@
 def print_records(file_name: str):
     with open(file_name, 'r', encoding='utf-8') as data:
         for line in data:
-            print(*line.split(';'))
+            print(*line.split(';'), end='')
 
 
 def input_records(file_name: str):
